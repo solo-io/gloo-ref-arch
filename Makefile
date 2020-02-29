ad3103166273b4f577dbef8981753f23bccf8cb8
Rick Ducott <rick.ducott@solo.io>
2020-02-29 17:39:59 -0500

    added script for running all

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 .PHONY: gen-docs
 gen-docs:
 	go run scripts/gen-docs/main.go
+
+.PHONY: run-all
+run-all:
+	go run scripts/run-all/main.go
--- scripts/run-all/main.go
+++ scripts/run-all/main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"github.com/pkg/errors"
+	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
@@ -15,12 +20,11 @@ func main() {
 			return nil
 		}
 		if filepath.Base(path) == "workflow.yaml" {
-			cmd := exec.Command("valet", "ensure", "-f", "workflow.yaml")
-			cmd.Dir = filepath.Dir(path)
-
-			out, err := cmd.CombinedOutput()
-			if err != nil {
-				log.Fatalf("Error: %v\n\n%s", err, out)
+			if err := Run("ensure", filepath.Dir(path)); err != nil {
+				return err
+			}
+			if err := Run("teardown", filepath.Dir(path)); err != nil {
+				return err
 			}
 			log.Printf("Successfully processed %s\n", path)
 		}
@@ -29,4 +33,62 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+}
+
+func Run(valetCmd, dir string) error {
+	if handler, err := Stream(valetCmd, dir); err != nil {
+		return err
+	} else if err := handler.StreamHelper(errors.Errorf("error running valet workflow")); err != nil {
+		return err
+	}
+	return nil
+}
+
+type CommandStreamHandler struct {
+	WaitFunc func() error
+	Stdout   io.Reader
+	Stderr   io.Reader
+}
+
+func (c *CommandStreamHandler) StreamHelper(inputErr error) error {
+	go func() {
+		stdoutScanner := bufio.NewScanner(c.Stdout)
+		for stdoutScanner.Scan() {
+			log.Println(stdoutScanner.Text())
+		}
+		if err := stdoutScanner.Err(); err != nil {
+			log.Println("reading stdout from current command context:", err)
+		}
+	}()
+	stderr, _ := ioutil.ReadAll(c.Stderr)
+	if err := c.WaitFunc(); err != nil {
+		log.Println(fmt.Sprintf("%s\n", stderr))
+		return inputErr
+	}
+	return nil
+}
+
+func Stream(valetCmd, dir string) (*CommandStreamHandler, error) {
+	cmd := exec.Command("valet", valetCmd, "-f", "workflow.yaml")
+	cmd.Dir = dir
+	outReader, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+	errReader, err := cmd.StderrPipe()
+	if err != nil {
+		return nil, err
+	}
+	err = cmd.Start()
+	if err != nil {
+		return nil, err
+	}
+	return &CommandStreamHandler{
+		WaitFunc: func() error {
+			return cmd.Wait()
+		},
+		Stdout: outReader,
+		Stderr: errReader,
+	}, nil
 }
