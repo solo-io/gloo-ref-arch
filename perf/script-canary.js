a7ce363208b802f4688ffbffa04d523d0773f36c
Rick Ducott <rick.ducott@solo.io>
2020-04-13 18:02:29 -0400

    Updates

--- perf/script-canary.js
+++ perf/script-canary.js
@@ -1,5 +1,10 @@
 import http from "k6/http";
 
 export default function() {
+    var params = {
+      headers: {
+        'stage': 'canary',
+      },
+    };
     let response = http.get("http://35.227.127.150:80");
 };
